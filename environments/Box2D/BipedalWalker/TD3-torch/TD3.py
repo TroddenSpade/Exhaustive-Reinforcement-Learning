e398a6c3e8e1c31881e41d5cdf19e9005029f57e
Parsa Samadnejad <troddenspade@gmail.com>
2021-08-23 20:20:10 +0430

    Create TD3.py

---
+++ environments/Box2D/BipedalWalker/TD3-torch/TD3.py
@@ -0,0 +1,92 @@
+import torch
+import pickle
+import matplotlib.pyplot as plt
+import os
+import sys
+
+PATH = os.path.dirname(__file__)
+FILE_PATH = os.path.join(PATH ,"TD3_res.pkl")
+GIF_PATH = os.path.join(PATH , "gifs")
+ROOT_PATH = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '../../../..'))
+sys.path.insert(0, ROOT_PATH)
+
+from code.DRL.DPG.TD3.torch import TD3
+from code.DRL.tools import DefineEnv
+
+if __name__ == "__main__":
+    n_runs = 3
+    n_episodes = 1000
+    args = {
+        "create_env_fn": DefineEnv("BipedalWalker-v3"),
+        "actor_kwargs":{
+            "hidden_layers":(400,300),
+            "activation_fn":torch.nn.functional.relu,
+            "out_activation_fn":torch.tanh,
+            "optimizer":torch.optim.Adam, 
+            "learning_rate":1e-4,
+            "grad_max_norm":float("inf"),
+        },
+        "critic_kwargs":{
+            "states_hidden_layers":(),
+            "shared_hidden_layers":(400,300),
+            "activation_fn":torch.nn.functional.relu,
+            "optimizer":torch.optim.Adam, 
+            "learning_rate":1e-4,
+            "grad_max_norm":float("inf")
+        },
+        "update_actor_step":2,
+        "gamma":0.99,
+        "tau":0.005,
+        "batch_size":100,
+        "noise":0.1,
+        "noise_":0.2,
+        "clip_":0.5,
+    }
+
+    agent = TD3(**args)
+    res = agent.iterate(n_runs, n_episodes)
+    agent.create_gifs(GIF_PATH)
+
+    file = open(FILE_PATH, "wb")
+    pickle.dump(res, file)
+    file.close()
+
+    # file = open(FILE_PATH,'br')
+    # res = pickle.load(file)
+
+    avg_times= torch.zeros((n_runs, n_episodes))
+    avg_rewards = torch.zeros((n_runs, n_episodes))
+
+    for r in range(n_runs):
+        avg_times[r] = torch.tensor([
+            torch.mean(res["times"][r][max(i-9,0):i+1]) for i in range(n_episodes)])
+        avg_rewards[r] = torch.tensor([
+            torch.mean(res["rewards"][r][max(i-99,0):i+1]) for i in range(n_episodes)])
+
+    smoothed_times_mean = torch.mean(avg_times, axis=0)
+    smoothed_times_min = torch.min(avg_times, axis=0)[0]
+    smoothed_times_max = torch.max(avg_times, axis=0)[0]
+
+    smoothed_rewards_mean = torch.mean(avg_rewards, axis=0)
+    smoothed_rewards_min = torch.min(avg_rewards, axis=0)[0]
+    smoothed_rewards_max = torch.max(avg_rewards, axis=0)[0]
+
+    plt.figure(1)
+    plt.plot(smoothed_times_min, 'y', linewidth=1)
+    plt.plot(smoothed_times_max, 'y', linewidth=1)
+    plt.plot(smoothed_times_mean, 'y', label='TD3', linewidth=2)
+    plt.fill_between(
+        torch.arange(len(smoothed_times_mean)), 
+        smoothed_times_min, smoothed_times_max, facecolor='y', alpha=0.2)
+
+    plt.figure(2)
+    plt.plot([0,1000],[300,300], '--', color='crimson')
+    plt.plot(smoothed_rewards_min, 'deepskyblue', linewidth=1)
+    plt.plot(smoothed_rewards_max, 'deepskyblue', linewidth=1)
+    plt.plot(smoothed_rewards_mean, 'deepskyblue', label='TD3', linewidth=2)
+    plt.fill_between(
+        torch.arange(len(smoothed_rewards_mean)), 
+        smoothed_rewards_min, smoothed_rewards_max, facecolor='deepskyblue', alpha=0.2)
+
+    plt.show()
